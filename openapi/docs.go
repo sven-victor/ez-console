453b6b332f0bbd8e4f1cccdafc70c41bd0e04233
Sven Victor <Sven.Victor@outlook.com>
2026-07-12 17:06:04 +0100

    tag: 1.18.0

--- openapi/docs.go
+++ openapi/docs.go
@@ -13311,7 +13311,7 @@ const docTemplate = `{
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
-	Version:          "v1.17.0",
+	Version:          "v1.18.0",
 	Host:             "",
 	BasePath:         "",
 	Schemes:          []string{},
